845311a1d2de907ac8a71bb045966a219caf542c
lanfeng <962063423@qq.com>
2021-01-26 06:45:36 +0800

    other

---
+++ src/main/java/com/example/demo/controller/OtherController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OtherController {
+
+    public void other(){
+        System.out.println("这个是other Other");
+    }
+}
